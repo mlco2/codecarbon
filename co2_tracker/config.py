d60464f0ae357b23769b09ed59aafda2bade2683
Kamal Goyal <goyal.kamal@bcg.com>
2020-08-12 14:12:33 -0400

    add ISO Codes and remove separate file

--- co2_tracker/config.py
+++ co2_tracker/config.py
@@ -9,7 +9,7 @@ cfg = {
     "geo_js_url": "https://get.geojs.io/v1/ip/geo.json",
     "cloud_emissions_path": "data/cloud/impact.csv",
     "private_infra_us_path": "data/private_infra/2016/us_emissions.json",
-    "private_infra_energy_mix_path": "data/private_infra/2016/energy_mix.json",
+    "private_infra_global_energy_mix_path": "data/private_infra/2016/global_energy_mix.json",
 }
 
 
