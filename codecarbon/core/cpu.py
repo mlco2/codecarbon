3df0c9896f0ada0f5c4653a04ad7ef545bf43efb
Boris Feld <lothiraldan@gmail.com>
2020-11-23 19:16:46 +0100

    Fix subprocess escaping for Windows and likely Mac

--- codecarbon/core/cpu.py
+++ codecarbon/core/cpu.py
@@ -73,16 +73,20 @@ class IntelPowerGadget:
         """
         Logs output from Intel Power Gadget command line to a file
         """
-        if self._system.startswith("win"):
-            subprocess.call(
-                f"{self._cli} -duration {self._duration} -resolution {self._resolution} -file {self._log_file_path} > NUL 2>&1",
-                shell=True,
-            )
-        elif self._system.startswith("darwin"):
-            subprocess.call(
-                f"'{self._cli}' -duration {self._duration} -resolution {self._resolution} -file {self._log_file_path} > /dev/null",
-                shell=True,
-            )
+        returncode = subprocess.call(
+            [
+                self._cli,
+                "-duration",
+                str(self._duration),
+                "-resolution",
+                str(self._resolution),
+                "-file",
+                self._log_file_path,
+            ],
+            shell=True,
+            capture_output=True,
+        )
+        logger.debug("PowerLog returncode %d", returncode)
         return
 
     def get_cpu_details(self) -> Dict:
