38390fa2f88f2cb4101b296d597b361a88415c28
Aditya Joshi <1adityajoshi@gmail.com>
2020-11-26 15:14:05 -0500

    Use decorater instead of context managers to mock tests

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -14,7 +14,9 @@ class TestIntelPowerGadget(unittest.TestCase):
         cpu_details = power_gadget.get_cpu_details()
         assert len(cpu_details) > 0
 
-    def test_get_cpu_details(self):
+    @mock.patch("codecarbon.core.cpu.IntelPowerGadget._log_values")
+    @mock.patch("codecarbon.core.cpu.IntelPowerGadget._setup_cli")
+    def test_get_cpu_details(self, mock_setup, mock_log_values):
         expected_cpu_details = {
             "Unnamed: 0": 5.5,
             "CPU Utilization(%)": 10.019916666666667,
@@ -40,26 +42,23 @@ class TestIntelPowerGadget(unittest.TestCase):
             "GT Requsted Frequency(MHz)": 125.0,
         }
 
-        with mock.patch.object(
-            IntelPowerGadget, "_log_values", return_value=None
-        ) as _, mock.patch.object(
-            IntelPowerGadget, "_setup_cli", return_value=None
-        ) as _:
-            power_gadget = IntelPowerGadget(
-                output_dir=os.path.join(os.path.dirname(__file__), "test_data"),
-                log_file_name="mock_intel_power_gadget_data.csv",
-            )
-            cpu_details = power_gadget.get_cpu_details()
-            self.assertDictEqual(expected_cpu_details, cpu_details)
+        power_gadget = IntelPowerGadget(
+            output_dir=os.path.join(os.path.dirname(__file__), "test_data"),
+            log_file_name="mock_intel_power_gadget_data.csv",
+        )
+        cpu_details = power_gadget.get_cpu_details()
+        self.assertDictEqual(expected_cpu_details, cpu_details)
 
 
 class TestIntelRAPL(unittest.TestCase):
-    def test_intel_rapl(self):
+    @mock.patch(
+        "codecarbon.core.cpu.IntelRAPL._is_platform_supported",
+        return_value=True,
+    )
+    def test_intel_rapl(self, mock_platform):
         expected_cpu_details = {"Processor Power_0(Watt)": 0.0, "psys": 0.0}
-        with mock.patch.object(
-            IntelRAPL, "_is_platform_supported", return_value=True
-        ) as _:
-            rapl = IntelRAPL(
-                rapl_dir=os.path.join(os.path.dirname(__file__), "test_data", "rapl")
-            )
-            self.assertDictEqual(expected_cpu_details, rapl.get_cpu_details())
+
+        rapl = IntelRAPL(
+            rapl_dir=os.path.join(os.path.dirname(__file__), "test_data", "rapl")
+        )
+        self.assertDictEqual(expected_cpu_details, rapl.get_cpu_details())
