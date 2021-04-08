f07a48d96ff4fe4d7f9f6c267998ce975339c2a7
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-04-08 22:12:26 +0200

    WIP : Database schema

--- api/database/database.py
+++ api/database/database.py
@@ -1,3 +1,18 @@
 # TODO : read https://fastapi.tiangolo.com/tutorial/sql-databases/
 
 # Put here the creation of the database
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./code_carbon.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# We will use the function declarative_base() that returns a class.
+# Later we will inherit from this class to create each of the database models or classes (the ORM models)
+Base = declarative_base()
--- api/database/models.py
+++ api/database/models.py
@@ -1,3 +1,86 @@
 # TODO : read https://fastapi.tiangolo.com/tutorial/sql-databases/
 
 # Put here the structure of the database
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Emission(Base):
+    __tablename__ = "emissions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime)
+    # experiment_id = Column(Integer)
+    duration = Column(Float)
+    emissions = Column(Float)
+    energy_consumed = Column(Float)
+    country_name = Column(String)
+    country_iso_code = Column(String)
+    region = Column(String)
+    on_cloud = Column(Boolean, default=False)
+    cloud_provider = Column(String)
+    cloud_region = Column(String)
+    experiment_id = Column(Integer, ForeignKey("experiments.id"))
+    experiment = relationship("Experiment", back_populates="emissions")
+
+
+# Un experiment = un run
+class Experiment(Base):
+    __tablename__ = "experiments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime)
+    name = Column(String)
+    description = Column(String)
+    is_active = Column(Boolean, default=True)
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    emissions = relationship("Emission", back_populates="experiment")
+    project = relationship("Project", back_populates="experiments")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    experiments = relationship("Experiment", back_populates="project")
+    team = relationship("Team", back_populates="projects")
+
+
+class Team(Base):
+    __tablename__ = "teams"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    organization_id = Column(Integer, ForeignKey("organizations.id"))
+    projects = relationship("Project", back_populates="team")
+    organization = relationship("Organization", back_populates="teams")
+
+
+# Organization
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    teams = relationship("Team", back_populates="organization")
+
+
+
+# class User(Base):
+#     __tablename__ = "users"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
+#     email = Column(String, unique=True, index=True)
+#     hashed_password = Column(String)
+#     is_active = Column(Boolean, default=True)
+
+#     projects = relationship("Project", back_populates="team")
+#     organizations = relationship("Organization", back_populates="teams")
