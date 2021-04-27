daf8dcc5f8f6831a155154de56e7ecfd2cc8d3f3
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-04-27 22:39:33 +0200

    Fix flake8 and black

--- api/routers/projects.py
+++ api/routers/projects.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Path, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_token_header, get_db
-from database.Infra.SqlAlchemy import repository_projects
-from database.Infra.Domain.schemas import ProjectCreate
+from infra.repository.repository_projects import SqlAlchemyRepository
+from domain.schemas import ProjectCreate
 
 
 router = APIRouter(
@@ -17,24 +17,29 @@ projects_temp_db = []
 def add_project(project: ProjectCreate, db: Session = Depends(get_db)):
     # Remove next line when DB work
     projects_temp_db.append(project.dict())
-    crud_projects.save_project(db, project)
+    repository_projects = SqlAlchemyRepository(db)
+    repository_projects.add_save_emission(db, project)
 
 
 @router.get("/project/{project_id}", tags=["projects"])
 async def read_project(
     project_id: str = Path(..., title="The ID of the project to get")
 ):
-    project = crud_projects.get_one_project(project_id)
-    if project_id is False:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return project
+    # project = crud_projects.get_one_project(project_id)
+    # if project_id is False:
+    #     raise HTTPException(status_code=404, detail="Item not found")
+    # return project
+    raise HTTPException(status_code=501, detail="Not Implemented")
+    return
 
 
 @router.get("/projects/{project_id}", tags=["projects"])
 async def read_experiment_projects(
     experiment_id: str = Path(..., title="The ID of the experiment to get")
 ):
-    experiment_projects = crud_projects.get_projects_from_experiment(experiment_id)
-    # Remove next line when DB work
-    experiment_projects = projects_temp_db
-    return experiment_projects
+    # experiment_projects = crud_projects.get_projects_from_experiment(experiment_id)
+    # # Remove next line when DB work
+    # experiment_projects = projects_temp_db
+    # return experiment_projects
+    raise HTTPException(status_code=501, detail="Not Implemented")
+    return
--- api/routers/teams.py
+++ api/routers/teams.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Path, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_token_header, get_db
-from database.Infra.SqlAlchemy import repository_teams
-from database.Infra.Domain.schemas import TeamCreate
+
+# from database.Infra.SqlAlchemy import repository_teams
+from domain.schemas import TeamCreate
 
 
 router = APIRouter(
@@ -16,23 +17,27 @@ teams_temp_db = []
 @router.put("/team", tags=["teams"])
 def add_team(team: TeamCreate, db: Session = Depends(get_db)):
     # Remove next line when DB work
-    teams_temp_db.append(team.dict())
-    crud_teams.save_team(db, team)
+    # teams_temp_db.append(team.dict())
+    # crud_teams.save_team(db, team)
+    raise HTTPException(status_code=501, detail="Not Implemented")
 
 
 @router.get("/team/{team_id}", tags=["teams"])
 async def read_team(team_id: str = Path(..., title="The ID of the team to get")):
-    team = crud_teams.get_one_team(team_id)
-    if team_id is False:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return team
+    # team = crud_teams.get_one_team(team_id)
+    # if team_id is False:
+    #     raise HTTPException(status_code=404, detail="Item not found")
+    # return team
+    raise HTTPException(status_code=501, detail="Not Implemented")
+    return
 
 
 @router.get("/teams/{team_id}", tags=["teams"])
 async def read_project_teams(
     project_id: str = Path(..., title="The ID of the project to get")
 ):
-    project_teams = crud_teams.get_Project_from_Teams(team_id)
-    # Remove next line when DB work
-    project_teams = teams_temp_db
-    return project_teams
+    # project_teams = crud_teams.get_Project_from_Teams(team_id)
+    # # Remove next line when DB work
+    # project_teams = teams_temp_db
+    # return project_teams
+    raise HTTPException(status_code=501, detail="Not Implemented")
