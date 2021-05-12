a1c68362e48e2ab4ea5c4fdf94203239f8ca245f
Kenzo-Hugo Hillion <hillion.kenzo@posteo.net>
2021-05-12 18:16:46 +0200

    run carbonserver with docker-compose
    fix pre-commit

--- carbonserver/carbonserver/database/database.py
+++ carbonserver/carbonserver/database/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./code_carbon.db"
+from carbonserver.config import settings
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./code_carbon.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.db_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # We will use the function declarative_base() that returns a class.
