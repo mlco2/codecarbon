c2b60743e3eb3a87d2bae4666fd07255de195090
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-05-19 15:21:16 +0200

    WIP : prepare docker for package

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-cd /opt/codecarbon
-set -e
-sleep 1
-# echo "$DATABASE_USER@$DATABASE_HOST:$DATABASE_PORT"
-# until PGPASSWORD=$DATABASE_PASS psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -p "$DATABASE_PORT" -d "$DATABASE_NAME" -c '\q'; do
-#   >&2 echo "Postgres is unavailable - sleeping"
-#   sleep 1
-# done
-  
-# >&2 echo "Postgres is up !"
-
-echo "codecarbon : Alembic migrate..."
-alembic upgrade head
-echo "codecarbon : starting..."
-python3 -m uvicorn main:app --reload
+# TODO : Run a Python code to use codecarbon package and call the API
