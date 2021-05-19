a95a00ab138184f43232343a55958c7d6e57fe42
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-05-19 15:18:14 +0200

    Fix requirements

--- carbonserver/setup.py
+++ carbonserver/setup.py
@@ -4,18 +4,27 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 DEPENDENCIES = [
-    "uvicorn[standard]==0.13.4",
-    "fastapi==0.63.0",
+    "uvicorn[standard]==0.13.4,<0.14",
+    "fastapi>=0.63.0,<0.7",
     "sqlalchemy",
+    "psycopg2-binary",
+    "pydantic[email]",
 ]
 
-TEST_DEPENDENCIES = ["mock", "pytest", "responses", "tox", "pydantic[email]"]
+TEST_DEPENDENCIES = [
+    "mock",
+    "pytest",
+    "requests",
+    "responses",
+    "tox",
+    "alembic",
+]
 
 
 setuptools.setup(
     name="carbonserver",
     version="0.1.0",
-    author="BCG GAMMA, Comet.ml, Haverford College, MILA",
+    author="BCG GAMMA, Comet.ml, Haverford College, MILA, DataForGood France",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(
