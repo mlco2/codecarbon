688149cba34c3dde161a07554504229b77b6c447
AlexisBogroff <alexis.bogroff@gmail.com>
2021-07-17 01:39:21 +0200

    Put graphs into cards and remove some titles

--- dash/sections.py
+++ dash/sections.py
@@ -48,8 +48,8 @@ def section_body(data, labels):
         children=[
             *subsection_timeseries(labels),
             card(subsection_metrics(data)),
-            *subsection_static_graphs(data),
-            *subsection_slider(data),
+            card(subsection_static_graphs(data)),
+            card(subsection_slider(data)),
         ],
         className="wrapper",
     )
@@ -76,13 +76,10 @@ def section_header():
 def subsection_metrics(data):
     """ General metrics on data """
     components = [
-        html.H1('Other Information :'),
-        html.H3('Total Energy Consumed :'),
-        html.H3(f"{data['energy_consumed'].sum()} kWh"),
-        html.H3('Total Emissions :'),
-        html.H3(f"{data['emissions'].sum()} kg"),
-        html.H3('Total duration :'),
-        html.H3(format_timespan(data['duration'].sum())),
+        html.H2('Metrics'),
+        html.H3(f"Total Energy Consumed : {round(data['energy_consumed'].sum(), 2)} kWh"),
+        html.H3(f"Total Emissions : {round(data['emissions'].sum(), 2)} kg"),
+        html.H3(f"Total duration : {format_timespan(data['duration'].sum())}"),
     ]
     return components
 
@@ -95,11 +92,11 @@ def subsection_timeseries(labels):
 def subsection_static_graphs(data):
     """ Add static graphs """
     graphs = [
-        html.H1('Energy Consumed'),
         graph('graph_conso_energy_period', fig_energy_by_day_period(data)),
         graph("graph_co2_energy", fig_co2_energy(data['energy_consumed'],
                                                  data['emissions'],
-                                                 data['timestamp']))
+                                                 data['timestamp'])),
+        html.Br(),
     ]
     return graphs
 
@@ -112,7 +109,6 @@ def subsection_slider(data):
     max_week = weeks.max()
 
     components = [
-        html.H1("Energy Consumed"),
         dcc.Graph(id='graph-with-slider'),
         dcc.Slider(id='week-slider',
                    min=min_week,
