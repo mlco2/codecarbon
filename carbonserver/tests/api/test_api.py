72a23655efb8f7d3b9271aa4a8a5771d79fe0dc8
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-07-18 15:55:19 +0200

    Add assert

--- carbonserver/tests/api/test_api.py
+++ carbonserver/tests/api/test_api.py
@@ -1,8 +1,12 @@
+import os
 import random
 
 import requests
 
-URL = "http://localhost:8008"
+# Get the API utl to use from an env variable if exist
+url = os.getenv("CODECARBON_API_URL")
+URL = url if url else "http://localhost:8008"
+
 experiment_id = project_id = user_id = api_key = org_id = team_id = email = None
 org_name = org_description = org_new_id = None
 team_name = team_description = team_new_id = emission_id = None
@@ -10,13 +14,26 @@ password = "Secret1!îstring"
 
 
 def is_key_value_exist(list_of_dict, key, value):
+    """
+    Check if at least one value of a key is equal to the specified value.
+    """
     for d in list_of_dict:
         if d[key] == value:
             return True
     return False
 
 
-def test_user_create():
+def is_key_all_values_equal(list_of_dict, key, value):
+    """
+    Check if all values of a key are equal to the specified value.
+    """
+    for d in list_of_dict:
+        if d[key] != value:
+            return False
+    return True
+
+
+def test_api_user_create():
     email = f"test-{random.randint(1, 20_000_000)}@test.com"
     payload = {"email": email, "name": "toto", "password": password}
     r = requests.post(url=URL + "/user", json=payload, timeout=2)
@@ -25,7 +42,7 @@ def test_user_create():
     assert r.json()["is_active"] == True  # noqa
 
 
-def test_user_signup():
+def test_api_user_signup():
     global user_id, api_key, org_id, team_id, email
     email = f"test-{random.randint(1, 20_000_000)}@test.com"
     payload = {"email": email, "name": "toto", "password": password}
@@ -39,19 +56,19 @@ def test_user_signup():
     team_id = r.json()["teams"][0]
 
 
-def test_users_list():
+def test_api_users_list():
     r = requests.get(url=URL + "/users", timeout=2)
     assert r.status_code == 200
     assert is_key_value_exist(r.json(), "id", user_id)
 
 
-def test_get_user():
+def test_api_get_user():
     r = requests.get(url=URL + "/user/" + user_id, timeout=2)
     assert r.status_code == 200
     assert r.json()["email"] == email
 
 
-def test_auth_success():
+def test_api_auth_success():
     payload = {"email": email, "password": password}
     r = requests.post(url=URL + "/authenticate/", json=payload, timeout=2)
     assert r.status_code == 200
@@ -59,13 +76,13 @@ def test_auth_success():
     assert r.json()["token_type"] == "access"
 
 
-def test_auth_fail():
+def test_api_auth_fail():
     payload = {"email": "toto@free.fr", "password": "password"}
     r = requests.post(url=URL + "/authenticate/", json=payload, timeout=2)
     assert r.status_code == 401
 
 
-def test_organization_create():
+def test_api_organization_create():
     global org_name, org_description, org_new_id
     org_name = "test_to_delete"
     org_description = "test to delete"
@@ -77,20 +94,20 @@ def test_organization_create():
     org_new_id = r.json()["id"]
 
 
-def test_organization_read():
+def test_api_organization_read():
     r = requests.get(url=URL + "/organization/" + org_new_id, timeout=2)
     assert r.status_code == 200
     assert r.json()["name"] == org_name
     assert r.json()["description"] == org_description
 
 
-def test_organization_list():
+def test_api_organization_list():
     r = requests.get(url=URL + "/organizations", timeout=2)
     assert r.status_code == 200
     assert r.json()[1]["id"] == org_id
 
 
-def test_team_create():
+def test_api_team_create():
     global team_name, team_description, team_new_id
     team_name = "test_to_delete"
     team_description = "test to delete"
@@ -107,20 +124,20 @@ def test_team_create():
     team_new_id = r.json()["id"]
 
 
-def test_team_read():
+def test_api_team_read():
     r = requests.get(url=URL + "/team/" + team_new_id, timeout=2)
     assert r.status_code == 200
     assert r.json()["name"] == team_name
     assert r.json()["description"] == team_description
 
 
-def test_teams_list():
+def test_api_teams_list():
     r = requests.get(url=URL + "/teams", timeout=2)
     assert r.status_code == 200
-    is_key_value_exist(r.json(), "id", team_id)
+    assert is_key_value_exist(r.json(), "id", team_id)
 
 
-def test_project_create():
+def test_api_project_create():
     global project_id
     payload = {
         "name": "test_to_delete",
@@ -134,13 +151,13 @@ def test_project_create():
 
 
 # Not implemented yet
-# def test_projects_list():
+# def test_api_projects_list():
 #     r = requests.get(url=URL + "/projects", timeout=2)
 #     assert r.status_code == 200
 #     assert is_key_value_exist(r.json(),"id",project_id)
 
 
-def test_experiment_create():
+def test_api_experiment_create():
     global experiment_id
     payload = {
         "name": "Run on Premise",
@@ -160,18 +177,19 @@ def test_experiment_create():
     experiment_id = r.json()["id"]
 
 
-def test_experiment_read():
+def test_api_experiment_read():
     r = requests.get(url=URL + "/experiment/" + experiment_id, timeout=2)
     assert r.status_code == 200
+    assert r.json()["id"] == experiment_id
 
 
-def test_experiment_list():
+def test_api_experiment_list():
     r = requests.get(url=URL + "/experiments/project/" + project_id, timeout=2)
     assert r.status_code == 200
     assert is_key_value_exist(r.json(), "id", experiment_id)
 
 
-def test_run_create():
+def test_api_run_create():
     global run_id
     payload = {"timestamp": "2021-04-04T08:43:00+02:00", "experiment_id": experiment_id}
     r = requests.post(url=URL + "/run/", json=payload, timeout=2)
@@ -179,19 +197,19 @@ def test_run_create():
     run_id = r.json()["id"]
 
 
-def test_run_read():
+def test_api_run_read():
     r = requests.get(url=URL + "/run/" + run_id, timeout=2)
     assert r.status_code == 200
     assert r.json()["id"] == run_id
 
 
-def test_run_list():
+def test_api_run_list():
     r = requests.get(url=URL + "/runs", timeout=2)
     assert r.status_code == 200
     assert is_key_value_exist(r.json(), "id", run_id)
 
 
-def test_emission_create():
+def test_api_emission_create():
     payload = {
         "timestamp": "2021-04-04T08:43:00+02:00",
         "run_id": run_id,
@@ -203,13 +221,16 @@ def test_emission_create():
     assert r.status_code == 201
 
 
-def test_emission_list():
+def test_api_emission_list():
     global emission_id
     r = requests.get(url=URL + "/emissions/run/" + run_id, timeout=2)
     assert r.status_code == 200
+    assert is_key_all_values_equal(r.json(), "run_id", run_id)
     emission_id = r.json()[-1]["id"]
 
 
-def test_emission_read():
+def test_api_emission_read():
     r = requests.get(url=URL + "/emission/" + emission_id, timeout=2)
     assert r.status_code == 200
+    assert r.json()["id"] == emission_id
+    assert r.json()["run_id"] == run_id
