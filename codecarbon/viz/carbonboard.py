8d10f854ec5bc4a2322335f9407b5bbb35fd81e1
AlexisBogroff <alexis.bogroff@gmail.com>
2021-08-03 20:09:45 +0200

    Move import dash

--- codecarbon/viz/carbonboard.py
+++ codecarbon/viz/carbonboard.py
@@ -1,3 +1,4 @@
+import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_table as dt
@@ -5,7 +6,6 @@ import fire
 import pandas as pd
 from dash.dependencies import Input, Output
 
-import dash
 from codecarbon.viz.components import Components
 from codecarbon.viz.data import Data
 
