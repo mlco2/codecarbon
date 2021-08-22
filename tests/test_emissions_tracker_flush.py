f3ae6874fc788cea2873233a1cb18b02fe6a49f3
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-08-22 11:14:41 +0200

    Add test

--- tests/test_emissions_tracker_flush.py
+++ tests/test_emissions_tracker_flush.py
@@ -16,10 +16,10 @@ def heavy_computation(run_time_secs: int = 3):
         pass
 
 
-class TestCarbonTrackerConstant(unittest.TestCase):
+class TestCarbonTrackerFlush(unittest.TestCase):
     def setUp(self) -> None:
-        self.project_name = "project_TestCarbonTrackerConstant"
-        self.emissions_file = "emissions-test-TestCarbonTrackerConstant.csv"
+        self.project_name = "project_TestCarbonTrackerFlush"
+        self.emissions_file = "emissions-test-TestCarbonTrackerFlush.csv"
         self.emissions_path = tempfile.gettempdir()
         self.emissions_file_path = os.path.join(
             self.emissions_path, self.emissions_file
@@ -31,11 +31,13 @@ class TestCarbonTrackerConstant(unittest.TestCase):
         if os.path.isfile(self.emissions_file_path):
             os.remove(self.emissions_file_path)
 
-    def test_carbon_tracker_online_constant(self):
+    def test_carbon_tracker_online_flush(self):
         tracker = EmissionsTracker(
             output_dir=self.emissions_path, output_file=self.emissions_file
         )
         tracker.start()
+        heavy_computation(run_time_secs=1)
+        tracker.flush()
         heavy_computation(run_time_secs=1)
         emissions = tracker.stop()
         assert isinstance(emissions, float)
@@ -43,7 +45,7 @@ class TestCarbonTrackerConstant(unittest.TestCase):
         self.assertAlmostEqual(emissions, 6.262572537957655e-05, places=2)
         self.verify_output_file(self.emissions_file_path)
 
-    def test_carbon_tracker_offline_constant(self):
+    def test_carbon_tracker_offline_flush(self):
         tracker = OfflineEmissionsTracker(
             country_iso_code="USA",
             output_dir=self.emissions_path,
@@ -51,26 +53,30 @@ class TestCarbonTrackerConstant(unittest.TestCase):
         )
         tracker.start()
         heavy_computation(run_time_secs=1)
+        tracker.flush()
+        heavy_computation(run_time_secs=1)
         emissions = tracker.stop()
         assert isinstance(emissions, float)
         self.assertNotEqual(emissions, 0.0)
         self.assertAlmostEqual(emissions, 6.262572537957655e-05, places=2)
         self.verify_output_file(self.emissions_file_path)
 
-    def test_decorator_constant(self):
+    def test_decorator_flush(self):
         @track_emissions(
             project_name=self.project_name,
             output_dir=self.emissions_path,
             output_file=self.emissions_file,
         )
         def dummy_train_model():
+            # I don't know how to call flush() in decorator mode
             return 42
 
-        dummy_train_model()
+        res = dummy_train_model()
+        self.assertEqual(res, 42)
 
-        self.verify_output_file(self.emissions_file_path)
+        self.verify_output_file(self.emissions_file_path, 2)
 
-    def verify_output_file(self, file_path: str) -> None:
+    def verify_output_file(self, file_path: str, expected_lines=3) -> None:
         with open(file_path, "r") as f:
             lines = [line.rstrip() for line in f]
-        assert len(lines) == 2
+        assert len(lines) == expected_lines
