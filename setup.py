a864f6fe86f5661471755683844f2811195b15f6
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-03-29 21:48:27 +0200

    Remove Python 3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 DEPENDENCIES = [
     "arrow",
-    "dataclasses;python_version<'3.7'",
     "pandas",
     "pynvml",
     "requests",
@@ -20,7 +19,7 @@ TEST_DEPENDENCIES = ["mock", "pytest", "responses", "tox", "numpy", "requests-mo
 
 setuptools.setup(
     name="codecarbon",
-    version="2.0.0a3",
+    version="2.0.0a4",
     author="Mila, DataForGood, BCG GAMMA, Comet.ml, Haverford College",
     long_description=long_description,
     long_description_content_type="text/markdown",
