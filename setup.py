da6384d865a62d24b170798bd619c564f60e8441
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-05-10 21:38:52 +0200

    Bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ TEST_DEPENDENCIES = ["mock", "pytest", "responses", "tox", "numpy", "requests-mo
 
 setuptools.setup(
     name="codecarbon",
-    version="2.1.0",
+    version="2.1.1",
     author="Mila, DataForGood, BCG GAMMA, Comet.ml, Haverford College",
     long_description=long_description,
     long_description_content_type="text/markdown",
