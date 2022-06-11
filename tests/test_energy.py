b09d940e1139eea7afec25d4b825ffd6ec11b97e
asaboni <asaboni@buster.ai>
2022-06-11 18:48:58 +0200

    lint & pre-commit

--- tests/test_energy.py
+++ tests/test_energy.py
@@ -2,14 +2,17 @@ import os.path
 import unittest
 from os import path
 
-from codecarbon.core.units import Energy, Time
 from core.rapl import RAPLFile
+
+from codecarbon.core.units import Energy, Time
 
 
 class TestEnergy(unittest.TestCase):
     def setUp(self):
         self.mock_energy_data_filename = "mock_rapl_data.txt"
-        self.path = path.join(path.dirname(os.path.abspath(__file__)), "test_data/mock_rapl_data.txt")
+        self.path = path.join(
+            path.dirname(os.path.abspath(__file__)), "test_data/mock_rapl_data.txt"
+        )
 
         # Setup the RAPL File with a high energy level
         with open(self.path, "w") as f:
@@ -17,8 +20,7 @@ class TestEnergy(unittest.TestCase):
 
     def test_negative_delta_writes_no_delta(self):
         first_rapl_measure = RAPLFile(
-            name=self.mock_energy_data_filename,
-            path=self.path
+            name=self.mock_energy_data_filename, path=self.path
         )
 
         # Write a cumulated value lower than the initial one
