c956f211faffbb8fc329385e9bfc8ff852324181
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-06-13 22:44:40 +0200

    Add CPU load tracking

--- codecarbon/core/cpu.py
+++ codecarbon/core/cpu.py
@@ -12,6 +12,7 @@ from typing import Dict, Optional, Tuple
 
 import pandas as pd
 from rapidfuzz import fuzz, process, utils
+import psutil
 
 from codecarbon.core.rapl import RAPLFile
 from codecarbon.core.units import Time
@@ -54,6 +55,21 @@ def is_rapl_available() -> bool:
             "Not using the RAPL interface, an exception occurred while instantiating "
             + "IntelRAPL : %s",
             e,
+        )
+        return False
+
+
+def is_psutil_available():
+    try:
+        cpu_load = psutil.cpu_percent(interval=0.1)
+        if cpu_load > 0.0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger.debug(
+            "Not using the psutil interface, an exception occurred while instantiating "
+            + f"psutil.cpu_percent : {e}",
         )
         return False
 
