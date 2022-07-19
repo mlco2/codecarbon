96c1ce15dbf33eaaaa378d3104bde64bfc9f1416
Martin O. Pollard <mp15@sanger.ac.uk>
2022-07-19 12:05:06 +0100

    Add max_energy_range_uj to deal with RAPL wraparound
    When we detect a negative delta from RAPL energy usage then assume
    we have wrapped around and add max_energy_range_uj to it.
    According to Intel docs this should happen about every 60 secs
    under a heavy load. Fixes #322

--- tests/test_energy.py
+++ tests/test_energy.py
@@ -12,20 +12,31 @@ class TestEnergy(unittest.TestCase):
         self.path = path.join(
             path.dirname(os.path.abspath(__file__)), "test_data/mock_rapl_data.txt"
         )
+        self.max_path = path.join(
+            path.dirname(os.path.abspath(__file__)), "test_data/mock_rapl_data_max.txt"
+        )
 
         # Setup the RAPL File with a high energy level
         with open(self.path, "w") as f:
             f.write("100")
+        # Setup the RAPL maximum value to be the energy level where we wrap
+        with open(self.max_path, "w") as f:
+            f.write("105")
 
-    def test_negative_delta_writes_no_delta(self):
+    def test_wraparound_delta_correct_value(self):
         first_rapl_measure = RAPLFile(
-            name=self.mock_energy_data_filename, path=self.path
+            name=self.mock_energy_data_filename, path=self.path, max_path=self.max_path
         )
 
+        # Read the initial state
+        first_rapl_measure.start()
         # Write a cumulated value lower than the initial one
         with open(self.path, "w") as f:
             f.write("10")
+        # Update state
         first_rapl_measure.delta(Time(seconds=1))
 
-        # Assert the delta didn't write a negative delta value
-        self.assertEqual(Energy(0), first_rapl_measure.energy_delta)
+        # Assert the delta has given the correct value from wraparound (reading + max_value - previous_value)
+        self.assertAlmostEqual(
+            Energy.from_ujoules(15).kWh, first_rapl_measure.energy_delta.kWh
+        )
