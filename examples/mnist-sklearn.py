cee7c343adc1045b8804762ef57db96b51a9fc19
Alexis Cruveiller <alexis.cruveiller@gmail.com>
2022-07-28 11:58:00 +0200

    Add Sklearn example
    I am working on Mac M1 meaning I have issues with `tensorflow`. For the sqke of simplicity I created a simple example using `sklearn`

--- examples/mnist-sklearn.py
+++ examples/mnist-sklearn.py
@@ -1,8 +1,9 @@
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
 
 from codecarbon import EmissionsTracker
+from codecarbon.external.logger import logger
 
 digits = datasets.load_digits()
 
@@ -10,7 +11,7 @@ digits = datasets.load_digits()
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier
+# Create a classifier: a support vector classifier
 model = MLPClassifier(
     hidden_layer_sizes=(128, 100),
     max_iter=1000,
@@ -26,6 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 tracker = EmissionsTracker()
 tracker.start()
+# Learn the digits on the train subset
 model.fit(X_train, y_train)
 emissions: float = tracker.stop()
 print(f"Emissions: {emissions} kg")
