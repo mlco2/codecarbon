722ab7fd4043d68dbb3d44cab6c3075b90de2629
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-10-04 21:45:27 +0200

    Fix path

--- dashboard/layout/app.py
+++ dashboard/layout/app.py
@@ -39,9 +39,7 @@ orga_id = df_org.id.unique().tolist()[1]
 df_project = get_project_list(orga_id)
 
 # Load WorldElectricityMix
-df_mix = pd.read_csv(
-    "https://raw.githubusercontent.com/mlco2/codecarbon/dashboard/dashboard/WorldElectricityMix.csv"
-)
+df_mix = pd.read_csv("./WorldElectricityMix.csv")
 
 # Define application
 app = dash.Dash(
