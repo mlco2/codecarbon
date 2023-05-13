7cdecff3fb8d6ac41e11d473dde13845c44b2cc5
benoit-cty <4-benoit-cty@users.noreply.git.leximpact.dev>
2023-05-13 08:32:14 +0200

    Tests with 3.8 and 3.11

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
     package_data={
