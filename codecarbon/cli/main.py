951ed7c199807490b054a3f227bb88a7c9dcc66c
LuisBlanche <loulouserr@gmail.com>
2024-01-13 17:34:47 +0100

    fix(cli): :art: add a questionary prompt function
    allows easier testing

--- codecarbon/cli/main.py
+++ codecarbon/cli/main.py
@@ -72,10 +72,11 @@ def init():
         )
         api = ApiClient(endpoint_url=api_endpoint)
         organizations = api.get_list_organizations()
-        org = questionary.select(
+        org = questionary_prompt(
             "Pick existing organization from list or Create new organization ?",
             [org["name"] for org in organizations] + ["Create New Organization"],
-        ).ask()
+            default="Create New Organization",
+        )
 
         if org == "Create New Organization":
             org_name = typer.prompt(
@@ -102,10 +103,11 @@ def init():
             organization = [orga for orga in organizations if orga["name"] == org][0]
         teams = api.list_teams_from_organization(organization["id"])
 
-        team = questionary.select(
+        team = questionary_prompt(
             "Pick existing team from list or create new team in organization ?",
             [team["name"] for team in teams] + ["Create New Team"],
-        ).ask()
+            default="Create New Team",
+        )
         if team == "Create New Team":
             team_name = typer.prompt("Team name", default="Code Carbon user test")
             team_description = typer.prompt(
@@ -123,11 +125,11 @@ def init():
         else:
             team = [t for t in teams if t["name"] == team][0]
         projects = api.list_projects_from_team(team["id"])
-        project = questionary.select(
+        project = questionary_prompt(
             "Pick existing project from list or Create new project ?",
             [project["name"] for project in projects] + ["Create New Project"],
             default="Create New Project",
-        ).ask()
+        )
         if project == "Create New Project":
             project_name = typer.prompt("Project name", default="Code Carbon user test")
             project_description = typer.prompt(
@@ -144,12 +146,12 @@ def init():
             project = [p for p in projects if p["name"] == project][0]
 
         experiments = api.list_experiments_from_project(project["id"])
-        experiment = questionary.select(
+        experiment = questionary_prompt(
             "Pick existing experiment from list or Create new experiment ?",
             [experiment["name"] for experiment in experiments]
             + ["Create New Experiment"],
             default="Create New Experiment",
-        ).ask()
+        )
         if experiment == "Create New Experiment":
             typer.echo("Creating new experiment")
             exp_name = typer.prompt(
@@ -157,7 +159,7 @@ def init():
             )
             exp_description = typer.prompt(
                 "Experiment description :",
-                default="Code Carbon user test",
+                default="Code Carbon user test ",
             )
 
             exp_on_cloud = Confirm.ask("Is this experiment running on the cloud ?")
@@ -191,13 +193,13 @@ def init():
         else:
             experiment_id = [e for e in experiments if e["name"] == experiment][0]["id"]
 
-    write_to_config = Confirm.ask(
-        "Write experiment_id to /.codecarbonconfig ? (Press enter to continue)"
-    )
+        write_to_config = Confirm.ask(
+            "Write experiment_id to /.codecarbonconfig ? (Press enter to continue)"
+        )
 
-    if write_to_config is True:
-        write_local_exp_id(experiment_id)
-        new_local = True
+        if write_to_config is True:
+            write_local_exp_id(experiment_id)
+            new_local = True
     typer.echo(
         "\nCodeCarbon Initialization achieved, here is your experiment id:\n"
         + click.style(f"{experiment_id}", fg="bright_green")
@@ -249,6 +251,15 @@ def monitor(
         # Infinite loop
         while True:
             time.sleep(300)
+
+
+def questionary_prompt(prompt, list_options, default):
+    value = questionary.select(
+        prompt,
+        list_options,
+        default,
+    ).ask()
+    return value
 
 
 if __name__ == "__main__":
