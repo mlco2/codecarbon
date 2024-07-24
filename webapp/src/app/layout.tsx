2d935f4458d68f487da5a1b84a94202383b3dc32
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-07-24 15:57:37 +0200

    First commit of the web app - half finished

---
+++ webapp/src/app/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { FiefAuthProvider } from "@fief/fief/nextjs/react";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <FiefAuthProvider currentUserPath="/api/current-user">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </FiefAuthProvider>
+      </body>
+    </html>
+  );
+}
