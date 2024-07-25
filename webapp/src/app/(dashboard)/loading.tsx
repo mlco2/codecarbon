78695dc58e49f199d152d9d07b560081cdbd504c
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-07-25 16:41:26 +0200

    Improved initial display

--- webapp/src/app/(dashboard)/loading.tsx
+++ webapp/src/app/(dashboard)/loading.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import { Loader2 } from "lucide-react";
-
-export default function Loader() {
-    return (
-        <div className="flex min-h-screen justify-center items-center flex-col">
-            <Loader2 className="h-16 w-16 animate-spin text-primary" />
-        </div>
-    );
-}
+export { default } from "@/components/loader"; // Adjust the relative path accordingly
