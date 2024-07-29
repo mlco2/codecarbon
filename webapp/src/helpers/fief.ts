c5e30df7dcf5b3d53115f3402219fa5dc6d88504
Patrick LLORET <plloret@hyperlex.fr>
2024-07-29 08:01:00 +0200

    config(webapp) edit auth/callback urls (#625)

--- webapp/src/helpers/fief.ts
+++ webapp/src/helpers/fief.ts
@@ -40,7 +40,7 @@ class MemoryUserInfoCache implements IUserInfoCache {
 export const fiefAuth = new FiefAuth({
     client: fiefClient,
     sessionCookieName: SESSION_COOKIE_NAME,
-    redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/auth-callback`,
+    redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`,
     returnToDefault: `${process.env.NEXT_PUBLIC_BASE_URL}/home?auth=true`,
     logoutRedirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     userInfoCache: new MemoryUserInfoCache(),
