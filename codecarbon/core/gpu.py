8ffd7c491c4fa3f3e08808d4e89333f6aaa1668a
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-08-08 12:58:28 +0200

    Added type hints and docstring to GPUDevice

--- codecarbon/core/gpu.py
+++ codecarbon/core/gpu.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Any, List, Dict, Union
 
 import pynvml
 
@@ -8,6 +9,22 @@ from codecarbon.external.logger import logger
 
 @dataclass
 class GPUDevice:
+    """
+    Represents a GPU device with associated energy and power metrics.
+
+    Attributes:
+        handle (any): An identifier for the GPU device.
+        gpu_index (int): The index of the GPU device in the system.
+        energy_delta (Energy): The amount of energy consumed by the GPU device
+            since the last measurement, expressed in kilowatt-hours (kWh).
+            Defaults to an initial value of 0 kWh.
+        power (Power): The current power consumption of the GPU device,
+            measured in watts (W). Defaults to an initial value of 0 W.
+        last_energy (Energy): The last recorded energy reading for the GPU
+            device, expressed in kilowatt-hours (kWh). This is used to
+            calculate `energy_delta`. Defaults to an initial value of 0 kWh.
+    """
+
     handle: any
     gpu_index: int
     # Energy consumed in kWh
@@ -17,14 +34,14 @@ class GPUDevice:
     # Last energy reading in kWh
     last_energy: Energy = field(default_factory=lambda: Energy(0))
 
-    def start(self):
+    def start(self) -> None:
         self.last_energy = self._get_energy_kwh()
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.last_energy = self._get_energy_kwh()
         self._init_static_details()
 
-    def _get_energy_kwh(self):
+    def _get_energy_kwh(self) -> Energy:
         return Energy.from_millijoules(self._get_total_energy_consumption())
 
     def delta(self, duration: Time) -> dict:
@@ -44,7 +61,7 @@ class GPUDevice:
             "power_usage": self.power,
         }
 
-    def get_static_details(self):
+    def get_static_details(self) -> Dict[str, Any]:
         return {
             "name": self._gpu_name,
             "uuid": self._uuid,
@@ -53,7 +70,7 @@ class GPUDevice:
             "gpu_index": self.gpu_index,
         }
 
-    def _init_static_details(self):
+    def _init_static_details(self) -> None:
         self._gpu_name = self._get_gpu_name()
         self._uuid = self._get_uuid()
         self._power_limit = self._get_power_limit()
@@ -61,7 +78,7 @@ class GPUDevice:
         memory = self._get_memory_info()
         self._total_memory = memory.total
 
-    def get_gpu_details(self):
+    def get_gpu_details(self) -> Dict[str, Any]:
         # Memory
         memory = self._get_memory_info()
 
@@ -82,26 +99,26 @@ class GPUDevice:
         }
         return device_details
 
-    def _to_utf8(self, str_or_bytes):
+    def _to_utf8(self, str_or_bytes) -> Any:
         if hasattr(str_or_bytes, "decode"):
             return str_or_bytes.decode("utf-8", errors="replace")
 
         return str_or_bytes
 
-    def _get_total_energy_consumption(self):
+    def _get_total_energy_consumption(self) -> int:
         """Returns total energy consumption for this GPU in millijoules (mJ) since the driver was last reloaded
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g732ab899b5bd18ac4bfb93c02de4900a
         """
         return pynvml.nvmlDeviceGetTotalEnergyConsumption(self.handle)
 
-    def _get_gpu_name(self):
+    def _get_gpu_name(self) -> Any:
         """Returns the name of the GPU device
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1ga5361803e044c6fdf3b08523fb6d1481
         """
         name = pynvml.nvmlDeviceGetName(self.handle)
         return self._to_utf8(name)
 
-    def _get_uuid(self):
+    def _get_uuid(self) -> Any:
         """Returns the globally unique GPU device UUID
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g72710fb20f30f0c2725ce31579832654
         """
@@ -114,19 +131,19 @@ class GPUDevice:
         """
         return pynvml.nvmlDeviceGetMemoryInfo(self.handle)
 
-    def _get_temperature(self):
+    def _get_temperature(self) -> int:
         """Returns degrees in the Celsius scale
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g92d1c5182a14dd4be7090e3c1480b121
         """
         return pynvml.nvmlDeviceGetTemperature(self.handle, pynvml.NVML_TEMPERATURE_GPU)
 
-    def _get_power_usage(self):
+    def _get_power_usage(self) -> int:
         """Returns power usage in milliwatts
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g7ef7dff0ff14238d08a19ad7fb23fc87
         """
         return pynvml.nvmlDeviceGetPowerUsage(self.handle)
 
-    def _get_power_limit(self):
+    def _get_power_limit(self) -> Union[int, None]:
         """Returns max power usage in milliwatts
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g263b5bf552d5ec7fcd29a088264d10ad
         """
@@ -141,13 +158,13 @@ class GPUDevice:
         """
         return pynvml.nvmlDeviceGetUtilizationRates(self.handle).gpu
 
-    def _get_compute_mode(self):
+    def _get_compute_mode(self) -> int:
         """Returns the compute mode of the GPU
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceEnumvs.html#group__nvmlDeviceEnumvs_1gbed1b88f2e3ba39070d31d1db4340233
         """
         return pynvml.nvmlDeviceGetComputeMode(self.handle)
 
-    def _get_compute_processes(self):
+    def _get_compute_processes(self) -> List:
         """Returns the list of processes ids having a compute context on the
         device with the memory used
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g46ceaea624d5c96e098e03c453419d68
@@ -159,7 +176,7 @@ class GPUDevice:
         except pynvml.NVMLError:
             return []
 
-    def _get_graphics_processes(self):
+    def _get_graphics_processes(self) -> List:
         """Returns the list of processes ids having a graphics context on the
         device with the memory used
         https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g7eacf7fa7ba4f4485d166736bf31195e
@@ -173,7 +190,7 @@ class GPUDevice:
 
 
 class AllGPUDevices:
-    def __init__(self):
+    def __init__(self) -> None:
         if is_gpu_details_available():
             logger.debug("GPU available. Starting setup")
             self.device_count = pynvml.nvmlDeviceGetCount()
@@ -186,7 +203,7 @@ class AllGPUDevices:
             gpu_device = GPUDevice(handle=handle, gpu_index=i)
             self.devices.append(gpu_device)
 
-    def get_gpu_static_info(self):
+    def get_gpu_static_info(self) -> List:
         """Get all GPUs static information.
         >>> get_gpu_static_info()
         [
@@ -210,7 +227,7 @@ class AllGPUDevices:
             logger.warning("Failed to retrieve gpu static info", exc_info=True)
             return []
 
-    def get_gpu_details(self):
+    def get_gpu_details(self) -> List:
         """Get all GPUs instantaneous metrics
         >>> get_gpu_details()
         [
@@ -242,7 +259,7 @@ class AllGPUDevices:
             logger.warning("Failed to retrieve gpu information", exc_info=True)
             return []
 
-    def get_delta(self, last_duration: Time):
+    def get_delta(self, last_duration: Time) -> List:
         """Get difference since last time this function was called
         >>> get_delta()
         [
@@ -266,7 +283,7 @@ class AllGPUDevices:
             return []
 
 
-def is_gpu_details_available():
+def is_gpu_details_available() -> bool:
     """Returns True if the GPU details are available."""
     try:
         pynvml.nvmlInit()
