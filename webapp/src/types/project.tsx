4c10483a319e7135a041c4be8bdb3dc1e4d7365f
inimaz <93inigo93@gmail.com>
2024-09-03 22:04:04 +0200

    temp
    Signed-off-by: inimaz <93inigo93@gmail.com>

--- webapp/src/types/project.tsx
+++ webapp/src/types/project.tsx
@@ -1,6 +1,15 @@
 export interface Project {
-  id: string;
-  name: string;
-  description: string;
-  organizationId: string;
+    id: string;
+    name: string;
+    description: string;
+    organizationId: string;
+}
+
+export interface IProjectToken {
+    id: string;
+    project_id: string;
+    last_used: string | null;
+    name: string;
+    token: string;
+    access: number;
 }
