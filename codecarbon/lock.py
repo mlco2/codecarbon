e4eb5f554c1a7b2f2128f60a35b6853e7d1134de
inimaz <93inigo93@gmail.com>
2024-11-01 10:42:40 +0100

    fix: lock release when there is a python error
    Signed-off-by: inimaz <93inigo93@gmail.com>

--- codecarbon/lock.py
+++ codecarbon/lock.py
@@ -4,8 +4,10 @@ It creates a lock file in /tmp/.codecarbon.lock and removes it on exit.
 If the lock file already exists, it exits the program.
 """
 
+import atexit
 import errno
 import os
+import signal
 import tempfile
 
 from codecarbon.external.logger import logger
@@ -19,6 +21,19 @@ class Lock:
 
     def __init__(self):
         self._has_created_lock = False
+
+        atexit.register(
+            self.release
+        )  # Ensure release() is called on unexpected exit of the user's python code
+        # Register signal handlers to ensure lock release on interruption
+        signal.signal(signal.SIGINT, self._handle_exit)  # Ctrl+C
+        signal.signal(signal.SIGTERM, self._handle_exit)  # Termination signal
+
+    def _handle_exit(self, signum, frame):
+        """Ensures the lock file is removed when the script is interrupted."""
+        logger.debug(f"Signal {signum} received. Releasing lock and exiting.")
+        self.release()
+        os._exit(1)  # Exit immediately to prevent further execution
 
     def acquire(self):
         """Creates a lock file and ensures it's the only instance running."""
@@ -41,6 +56,6 @@ class Lock:
             if self._has_created_lock:
                 os.remove(LOCKFILE)
         except OSError as e:
-            logger.error("Error:", e)
+            logger.debug(f"Error: {e}")
             if e.errno != errno.ENOENT:
                 raise
