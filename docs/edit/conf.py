46789d305673a2beb601239bbc2b95b58ed86242
Amine Saboni <43726203+SaboniAmine@users.noreply.github.com>
2025-01-19 20:07:39 +0100

    chore: bump version (#765)

--- docs/edit/conf.py
+++ docs/edit/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, BCG GAMMA, Comet.ml, Haverford College, MILA"
 author = "BCG GAMMA, Comet.ml, Haverford College, MILA, Data For Good"
 
 # The full version, including alpha/beta/rc tags
-release = "2.8.2"
+release = "2.8.3"
 
 
 # -- General configuration ---------------------------------------------------
