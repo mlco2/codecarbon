cde578cea2897dcc4c0509a8cd54337674fcf20a
benoit-cty <4-benoit-cty@users.noreply.git.leximpact.dev>
2025-04-06 11:21:56 +0200

    bump and docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,9 +1,13 @@
 const DOCUMENTATION_OPTIONS = {
 <<<<<<< HEAD
+<<<<<<< HEAD
     VERSION: '2.8.4',
 =======
     VERSION: '3.0.0_rc3',
 >>>>>>> d4fea24d (rebuild docs)
+=======
+    VERSION: '3.0.0_rc5',
+>>>>>>> 56e6aa27 (bump and docs)
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
