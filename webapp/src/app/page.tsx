a17aaa64e6cbd8aae4fe9d14967fcaae877a9629
inimaz <93inigo93@gmail.com>
2025-06-20 09:34:40 +0200

    fix: html semantics + less verbose

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -3,48 +3,49 @@ import { LogIn } from "lucide-react";
 
 export default async function Home() {
     return (
-        <main className="flex min-h-[100dvh] flex-col justify-between items-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-            <div className="mx-auto text-center mt-32 sm:mt-48">
-                <h1 className="text-4xl font-semi-bold tracking-tight text-foreground sm:text-5xl">
-                    Welcome to Code Carbon!
-                </h1>
-                <p className="mt-4 text-lg text-muted-foreground">
-                    Get started by signing in with your preferred service.
-                </p>
-                <div className="mt-6">
-                    <a
-                        href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?redirect=${process.env.NEXT_PUBLIC_BASE_URL}/home?auth=true`}
-                    >
-                        <Button className="w-full max-w-[300px]">
-                            <LogIn className="mr-2 h-5 w-5" />
-                            Sign in or create an account
-                        </Button>
-                    </a>
+        <>
+            <main className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+                <div className="mx-auto text-center">
+                    <h1 className="text-4xl font-semi-bold tracking-tight text-foreground sm:text-5xl">
+                        Welcome to Code Carbon!
+                    </h1>
+                    <p className="mt-4 text-lg text-muted-foreground">
+                        Get started by signing in with your preferred service.
+                    </p>
+                    <div className="mt-6">
+                        <a
+                            href={`${process.env.NEXT_PUBLIC_API_URL}/auth/login?redirect=${process.env.NEXT_PUBLIC_BASE_URL}/home?auth=true`}
+                        >
+                            <Button className="w-full max-w-[300px]">
+                                <LogIn className="mr-2 h-5 w-5" />
+                                Sign in or create an account
+                            </Button>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className="mx-auto text-center mb-4">
-                <p className="mt-4 text-sm text-muted-foreground">
-                    New to Code Carbon? More info about the project can be found
-                    at{" "}
+            </main>
+            <footer className="mx-auto text-center mb-4 text-sm text-muted-foreground space-y-2">
+                <p>
+                    New to Code Carbon? Learn more at{" "}
                     <a
                         href="https://codecarbon.io"
                         className="text-primary underline"
                     >
-                        https://codecarbon.io
+                        codecarbon.io
                     </a>
                     .
                 </p>
-                <p className="mt-4 text-sm text-muted-foreground">
-                    To contribute, check out our{" "}
+                <p>
+                    Want to contribute? Visit our{" "}
                     <a
                         href="https://github.com/mlco2/codecarbon"
                         className="text-primary underline"
                     >
-                        Github repository
+                        GitHub repository
                     </a>
                     .
                 </p>
-            </div>
-        </main>
+            </footer>
+        </>
     );
 }
