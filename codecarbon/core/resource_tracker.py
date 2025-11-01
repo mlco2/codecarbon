752699c4e16607ccd77c66d482e9f7941fb5bd82
benoit-cty <4-benoit-cty@users.noreply.git.leximpact.dev>
2025-11-01 10:43:11 +0100

    Better message

--- codecarbon/core/resource_tracker.py
+++ codecarbon/core/resource_tracker.py
@@ -123,7 +123,7 @@ class ResourceTracker:
             elif is_windows_os():
                 cpu_tracking_install_instructions = "Windows OS detected: Please install Intel Power Gadget to measure CPU"
             elif is_linux_os():
-                cpu_tracking_install_instructions = "Linux OS detected: Please ensure RAPL files exist at /sys/class/powercap/intel-rapl/subsystem to measure CPU"
+                cpu_tracking_install_instructions = "Linux OS detected: Please ensure RAPL files exist, and are readable, at /sys/class/powercap/intel-rapl/subsystem to measure CPU"
             logger.warning(
                 f"No CPU tracking mode found. Falling back on estimation based on TDP for CPU. \n {cpu_tracking_install_instructions}\n"
             )
