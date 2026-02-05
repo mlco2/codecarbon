0d06e9c52d858d6ce3a19979e84c6bf86bc0a901
Arno <arno@online.de>
2026-02-05 17:51:47 +0100

    fix: improve CPU detection on Windows and enhance psutil availability check to work on Windows as well

--- codecarbon/core/util.py
+++ codecarbon/core/util.py
@@ -103,7 +103,10 @@ def count_physical_cpus():
     import subprocess
 
     if platform.system() == "Windows":
-        return int(os.environ.get("NUMBER_OF_PROCESSORS", 1))
+        # Windows does not have a straightforward way to count physical CPUs (sockets).
+        # Env variable NUMBER_OF_PROCESSORS gives logical processors, not physical sockets.
+        # return int(os.environ.get("NUMBER_OF_PROCESSORS", 1))
+        return _windows_get_physical_sockets()
     else:
         try:
             output = subprocess.check_output(["lscpu"], text=True)
@@ -118,7 +121,26 @@ def count_physical_cpus():
             )
             return 1
 
+def _windows_get_physical_sockets():
+    try:
+        # use PowerShell to count number of objects of class Win32_Processor
+        cmd = ["powershell", "-NoProfile", "-Command", "(Get-CimInstance -ClassName Win32_Processor).Count"]      
+        result = subprocess.run(cmd, capture_output=True, text=True, timeout=10, check=True)
 
+        output = result.stdout.strip()
+
+        # Fallback: if Count is empty, at least one socket exists
+        if not output:
+            logger.debug("PowerShell command returned empty output for socket count. Defaulting to 1.")
+            output = 1           
+
+        logger.debug(f"Detected {output} physical sockets on Windows.")
+        return int(output)
+
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired, ValueError) as e:
+        logger.error(f"Error detecting physical sockets on Windows: {e}")
+        return 1  # Fallback:at least one socket
+    
 def count_cpus() -> int:
     if SLURM_JOB_ID is None:
         return psutil.cpu_count()
