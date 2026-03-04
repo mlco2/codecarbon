23ba32b436d8173c2f70953b1ae6c4579fa79429
Simon Decostanzi <gbsny8zh@gmail.com>
2026-03-04 20:33:31 +0100

    Enabled database changes migration

--- carbonserver/carbonserver/database/alembic/env.py
+++ carbonserver/carbonserver/database/alembic/env.py
@@ -22,7 +22,14 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+#target_metadata = None
+
+from carbonserver.database.database import Base
+from carbonserver.api.infra.database import sql_models 
+
+target_metadata = Base.metadata
+
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
