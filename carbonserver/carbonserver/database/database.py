a8f6a451bd3bc7be8e9bc7d3e2631909385e58c8
benoit-cty <hann@ony.me>
2026-07-28 08:27:35 +0200

    Fix code after deps bump

--- carbonserver/carbonserver/database/database.py
+++ carbonserver/carbonserver/database/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from carbonserver.config import settings
 
